873036f5f6ef520777b3197d1fc8f6e96203e1af
Tairan Wang <tairan.wang@gmail.com>
2018-06-17 01:45:44 +0800

    initial

---
+++ gatsby-config.js
@@ -0,0 +1,6 @@
+module.exports = {
+  siteMetadata: {
+    title: 'TAIRAN\'s Story',
+  },
+  plugins: ['gatsby-plugin-react-helmet'],
+}
---
+++ src/pages/index.js
@@ -0,0 +1,17 @@
+import React from 'react'
+import Link from 'gatsby-link'
+
+const IndexPage = () => (
+  <div>
+    <h1>Hi Gatsby</h1>
+    <p>Welcome</p>
+    <p>是时候重启博客了，在停止更新多年以后，我还是觉得应该有个自己的地方来记录关于我自己的故事。</p>
+    <p>时间没有停止过，静态内容站点已经进化到了<a href="https://headlesscms.org/">Headless CMS</a>, 作为自诩为极客的人，怎么能够错过这波浪潮呢？
+    你现在访问的这个站点就是一个基于Gatsby的Headless CMS，你可以参考这篇文章来了解我的选择, <a href="https://www.gatsbyjs.org/blog/2018-2-3-sites-with-headless-cms/">Building Sites with Headless CMSs</a>。</p>
+
+    <p>这只是个开始，接下来跟我一起构建Gatsby站点吧！</p>
+
+  </div>
+)
+
+export default IndexPage
